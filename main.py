33f97338c3106da787e350833dc6c3a2a495ffc0
@saiteja-a <bharanisai9090@gmail.com>
2026-09-19 19:47:15 +0530

    Added booking

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 import sqlite3
+from pydantic import BaseModel, Field
+from typing import Annotated, Literal
+from fastapi.responses import JSONResponse
+import random
+import string
+
+def generate_appointment_code():
+    return f"APT-{random.randint(1000, 9999)}"
 
 def connectdb():
     connection = sqlite3.Connection("appointments.db")
     cursor = connection.cursor()
-    return cursor
+    return [cursor, connection]
+
+class DocSlot(BaseModel):
+    doc_id: Annotated[str, Field(description="Provide the doctor's Id")]
+    date: Annotated[str,Field(description="Provide date of booking in format YYYY-MM-DD example 2026-09-25")]
+    slot: Annotated[str,Field(description="Provide the slot that user selected example 9AM-9:20AM etc")]
+
     
 app = FastAPI()
 
@@ -25,12 +39,32 @@ def get_appointment(appointment_id: str):
 @app.get("/all_doctor")
 def get_docs():
     cursor = connectdb()
+    cursor = cursor[0]
     data = cursor.execute("""
                    select * from DocDetails
                    """)
     doc_data = data.fetchall()
     return doc_data
+
+@app.post("/book_appointment")
+def book_appointment(request: DocSlot):
+    request_data = request.model_dump()
+    req_doc_id = request_data["doc_id"]
+    req_date = request_data["date"]
+    req_slot = request_data["slot"]
+    appointment_id = generate_appointment_code()
+    # response = {"doc_id":doc_id,"date": date,"slot": slot}
+    # return response
+    connection = sqlite3.Connection("appointments.db")
+    cursor = connection.cursor()
+    cursor.execute(f"""
+                   update DocSlots set "{req_slot}" = ? where date = ? and doc_id = ?
+                   """,(appointment_id,req_date,req_doc_id))
+    connection.commit()
+    connection.close()
+    return JSONResponse(status_code=200, content="Appointment booked successfully")
+    
+    
     
     
 
-    
