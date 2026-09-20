5273887e11fa078dbfab89b05beeccc9b5b9387f
@saiteja-a <bharanisai9090@gmail.com>
2026-09-20 10:57:32 +0530

    database connection

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from fastapi.responses import JSONResponse
 import random
 import string
 import os
+import psycopg2
+
+@app.get("/test-db")
+def test_db():
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    connection.close()
+    return {"message": "PostgreSQL connected successfully"}
 
 def generate_appointment_code():
     return f"APT-{random.randint(1000, 9999)}"
